de1657d048a3b27e314bb2e24ecd6c2eb3034762
Elielson André <elielson@voidseek.com>
2023-09-09 15:10:01 -0300

    chore: adicionando informações do perfil no header

--- src/components/chat/ChatHeader.vue
+++ src/components/chat/ChatHeader.vue
@@ -1,4 +1,30 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import MenuIcon from '../icons/MenuIcon.vue'
+
+// refs
+const isTyping = ref(false);
+
+</script>
 <template>
-    <header class="w-full bg-slate-600"></header>
+    <header class="w-full bg-slate-600 flex justify-between p-4">
+        <div class="flex items-center">
+            <div class="flex items-center">
+                <img class="w-11 h-11 rounded-full border-2 border-orange-600" src="../../images/turing.png"
+                    alt="Turing profile image">
+            </div>
+            <div class="text-white ml-2">
+                <p class="-mb-1">Turing - Assistente</p>
+                <small>
+                    <span v-if="isTyping">digitando...</span>
+                    <span v-else class="m-0 p-0 text-center flex items-center">
+                        <div class="w-2 h-2 rounded-full bg-green-300 mr-1"></div> online
+                    </span>
+                </small>
+            </div>
+        </div>
+        <div class="flex items-center">
+            <MenuIcon class="w-11 h-11 p-2 text-white  hover:bg-slate-700 hover:cursor-pointer rounded-full " />
+        </div>
+    </header>
 </template>
