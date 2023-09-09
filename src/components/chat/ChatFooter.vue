3e54263c9619440bef427961ee000c19fa0e353d
Elielson André <elielson@voidseek.com>
2023-09-09 15:40:28 -0300

    chore: adicionando input de mensagem do usuario

--- src/components/chat/ChatFooter.vue
+++ src/components/chat/ChatFooter.vue
@@ -1,4 +1,26 @@
 <script setup></script>
 <template>
-    <header class="w-full bg-slate-600"></header>
+    <header class="w-full bg-slate-600">
+        <!-- <div class="w-full h-full p-4 ">
+            <button class="bg-blue-500 rounded-md text-white w-full h-8 px-3">Iniciar</button>
+        </div> -->
+
+        <div class="w-full h-full p-2 flex items-center ">
+            <form class="w-full flex justify-around">
+                <input type="text" class="w-3/4 h-10 p-2 text-white rounded-md bg-slate-500 focus:outline-none"
+                    placeholder="Digite aqui...">
+                <button class="w-1/5 h-10 bg-blue-700 rounded-md text-white">
+                    Enviar
+                </button>
+            </form>
+        </div>
+
+    </header>
 </template>
+
+<style scoped>
+._input_message {
+    width: 80%;
+
+}
+</style>
