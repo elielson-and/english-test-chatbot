dec9883344d9572ee7e58dfe4ebcd31a8813f59e
Elielson André <elielson@voidseek.com>
2023-09-09 12:39:31 -0300

    add: adicionando view do chat e organizando componente

---
+++ src/components/ChatContainer.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+<template>
+    <div class="_chat_wrapper">
+        <div class="_chat_container">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+._chat_wrapper {
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(to bottom right, rgb(0, 7, 75), rgb(0, 15, 43));
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    justify-content: center;
+
+}
+
+._chat_container {
+    width: 100%;
+    max-width: 500px;
+    height: 100vh;
+    max-height: 600px;
+    border: 1px solid red
+}
+</style>
